dad930ab7ee0552a0ce3ce40f90fd43d55a2791e
Shubham Patial <patialshubham94@gmail.com>
2022-07-20 15:06:44 +0530

    changes done in web.php after merge 2 repo

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-<<<<<<< HEAD
-use App\Http\Controllers\User_loginMethods;
 
+use App\Http\Controllers\User_loginMethods;
+use App\Http\Controllers\B2BLeadGenerationController;
 
 
 Route::get('/', function () {
@@ -47,27 +47,13 @@ Route::get('/user', [User_loginMethods::class,'get_User'])->middleware('check')-
 
 // // Log In User
 // Route::post('/users/authenticate', [User_loginMethods::class, 'authenticate']);
-=======
-use App\Http\Controllers\B2BLeadGenerationController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+
 Route::get('/f', function () {
     return view('B2BLeadGeneration.form2');
 });
 
 Route::get('/', [B2BLeadGenerationController::class, 'createForm']);
 Route::post('/', [B2BLeadGenerationController::class, 'ContactUsForm'])->name('contact.store');
->>>>>>> newBranch
+
